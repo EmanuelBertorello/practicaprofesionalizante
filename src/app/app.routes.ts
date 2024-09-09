0f704f8a23dbfbdedd685edca7ffcfbf039180b9
rodrigo <rodrigalleguillos21@gmail.com>
2024-09-09 20:31:39 -0300

    rodri

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { StockComponent } from './stock/stock.component';
 import { RecetasComponent } from './recetas/recetas.component';
-import { InfoComponent } from './info/info.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { IndexComponent } from './index/index.component';
+import { SobrenosotrosComponent } from './sobrenosotros/sobrenosotros.component';
 
 export  const routes: Routes = [
   { path: 'stock', component: StockComponent,},
   {path: "recetas", component:RecetasComponent},
-  {path: "info", component:InfoComponent},
+  {path: "info", component:SobrenosotrosComponent},
   {path: "index", component:IndexComponent},
   {path: '**',component:NotfoundComponent}
 ];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: ["./src/**/*.{html,js}"],
-  content: [],
-=======
   content: [
     "./src/**/*.{html,ts}",
   ],
->>>>>>> 91a6adff066b7d469c97eb89cd650f90805854d7
   theme: {
     extend: {},
   },
   plugins: [],
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 91a6adff066b7d469c97eb89cd650f90805854d7
