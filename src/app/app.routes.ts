316cd1ec0362c3d2825bbebfbe59a94264eafd1b
rodrigo <rodrigalleguillos21@gmail.com>
2024-10-01 12:41:08 -0300

    01/10

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { IndexComponent } from './index/index.component';
 import { SobrenosotrosComponent } from './sobrenosotros/sobrenosotros.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { RegistroComponent } from './registro/registro.component';
+import { RecuperarContraComponent } from './recuperar-contra/recuperar-contra.component';
 
 export  const routes: Routes = [
   { path: 'stock', component: StockComponent,},
@@ -14,6 +15,7 @@ export  const routes: Routes = [
   {path: "index", component:IndexComponent},
   {path: "login", component:LogInComponent},
   {path: "registro", component:RegistroComponent},
+  {path: "recuperarcontra", component:RecuperarContraComponent},
   {path: '**',component:NotfoundComponent}
 ];
 
