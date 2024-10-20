1d09ccc517e584b88a9bf608b278558efdf28fa7
ema <ema-ber2011@live.com.ar>
2024-10-20 17:53:34 -0300

    conectacdo base de datos

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import {routes} from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
      provideRouter(routes),
-     provideClientHydration()
+     provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"escuelaespecial2031-373be","appId":"1:336317830793:web:39a563d4cc9c67e2a75858","storageBucket":"escuelaespecial2031-373be.appspot.com","apiKey":"AIzaSyCc2ZJFiwfWyUoTgtSLV-vEUzRvckNaAdg","authDomain":"escuelaespecial2031-373be.firebaseapp.com","messagingSenderId":"336317830793","measurementId":"G-EG6ECKG4KL"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
 @Component({
   selector: 'app-log-in',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.css'
 })
